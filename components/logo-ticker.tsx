16fe4f608470bf8f868a22d4ff2204377915fc29
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-01 22:25:12 +0530

    ticker animation

--- components/logo-ticker.tsx
+++ components/logo-ticker.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
@@ -6,7 +8,7 @@ import quantumLogo from "@/assets/logo-quantum.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import echoLogo from "@/assets/logo-echo.png";
 
-
+import { motion } from "framer-motion";
 
 const LogoTicker = () => {
     return (
@@ -17,15 +19,30 @@ const LogoTicker = () => {
                         <h2>Trusted by top innovative teams</h2>
                     </div>
                     <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-                        <div className="flex flex-none gap-14">
-                            {[acmeLogo, pulseLogo, celestialLogo, apexLogo, quantumLogo, echoLogo].map((logo) => (
+                        <motion.div
+                            initial={{
+                                translateX: "-50%"
+                            }}
+                            animate={{
+                                translateX: "0"
+                            }}
+                            transition={{
+                                repeat: Infinity,
+                                duration: 30,
+                                ease: "linear"
+                            }}
+                            className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+                        >
+                            {[acmeLogo, pulseLogo, celestialLogo, apexLogo, quantumLogo, echoLogo, 
+                            acmeLogo, pulseLogo, celestialLogo, apexLogo, quantumLogo, echoLogo
+                            ].map((logo) => (
                                 <img 
                                     src={logo.src}
                                     key={logo.src}
                                     className="h-6 w-auto"
                                 />
                             ))}
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
