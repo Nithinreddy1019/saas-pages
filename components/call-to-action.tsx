bd20a40ff3244f2e2751bbddf96608b2ca956115
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-02 13:52:40 +0530

    action animation

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -1,23 +1,86 @@
+"use client"
+
 import { Button } from "./ui/button";
 import starsbg from "@/assets/stars.png";
 import gridLines from "@/assets/grid-lines.png";
 
+import { motion, useMotionTemplate, useMotionValue, useScroll, useTransform } from "framer-motion";
+import { RefObject, useEffect, useRef } from "react";
+
+
+
+const useRelativeMousePositioning = (to: RefObject<HTMLElement>) => {
+    const mouseX = useMotionValue(0);
+    const mouseY = useMotionValue(0);
+
+    const updateMousePosition = (event: MouseEvent) => {
+        if(!to.current) return;
+        const { top, left } = to.current.getBoundingClientRect();
+
+        mouseX.set(event.x - left);
+        mouseY.set(event.y - top);
+    };
+
+    useEffect(() => {
+        window.addEventListener('mousemove', updateMousePosition);
+
+        return () => {
+            window.removeEventListener('mousemove', updateMousePosition);
+        }
+    }, [])
+
+    return [mouseX, mouseY]
+}
+
 
 
 export const CallToAction = () => {
+
+    const sectionref = useRef<HTMLElement>(null);
+    const borderedDivRef = useRef<HTMLDivElement>(null);
+
+    const { scrollYProgress } = useScroll({
+        target: sectionref,
+        offset: ["start end", "end start"]
+    });
+
+    const backgroundPositionY = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+
+    const [mouseX, mouseY] = useRelativeMousePositioning(borderedDivRef);
+
+    const maskImage = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px,black,transparent)`
+
     return (
-        <section className="py-20 md:py-24">
+        <section className="py-20 md:py-24" ref={sectionref}>
             <div className="container">
-                <div 
-                    className="border border-white/15 py-24 rounded-xl overflow-hidden relative" style={{
-                        backgroundImage: `url(${starsbg.src})`
+                <motion.div 
+                    ref={borderedDivRef}
+                    animate={{
+                        backgroundPositionX: starsbg.width,
+                    }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 60,
+                        ease: "linear"
+                    }}
+                    className="border border-white/15 py-24 rounded-xl overflow-hidden relative group" 
+                    style={{
+                        backgroundImage: `url(${starsbg.src})`,
+                        backgroundPositionY
                     }}
                 >
                     <div 
-                        className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]" style={{
+                        className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700" style={{
                             backgroundImage: `url(${gridLines.src})`
                         }}
                     ></div>
+                    <motion.div 
+                        className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay  opacity-0 group-hover:opacity-100 transition duration-700" style={{
+                            backgroundImage: `url(${gridLines.src})`,
+                            maskImage
+                        }}
+                    ></motion.div>
                     <div className="relative">
                         <h2 className="text-5xl md:text-6xl max-w-sm mx-auto tracking-tighter text-center font-medium">
                             AI-driven SEO for everyone.
@@ -32,7 +95,7 @@ export const CallToAction = () => {
                             </Button>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </section>
     )
