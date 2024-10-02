1ebb032b2dde28de9f4c52906f7b57c55bd3089d
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-10-02 13:24:28 +0530

    features animation

--- components/features.tsx
+++ components/features.tsx
@@ -3,7 +3,8 @@
 import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
 import Image from "next/image";
 import productImage from "@/assets/product-image.png";
-import { useRef } from "react";
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
+import { useMotionValue, useMotionTemplate, motion, animate, ValueAnimationTransition } from "framer-motion";
 
 const tabs = [
     {
@@ -34,9 +35,39 @@ const tabs = [
 
 
 
-const FeatureTab = (tab: typeof tabs[number]) => {
+const FeatureTab = (props: typeof tabs[number] & ComponentPropsWithoutRef<'div'> & { selected: boolean }) => {
 
+    const tabRef = useRef<HTMLDivElement>(null);
     const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+
+    const xPercentage = useMotionValue(0);
+    const yPercentage = useMotionValue(0);
+
+    const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}%,black,transparent)`
+
+
+    useEffect(() => {
+        if(!tabRef.current) return;
+
+        xPercentage.set(0);
+        yPercentage.set(0);
+
+        const { height, width } = tabRef.current?.getBoundingClientRect();
+        const circumference = height*2 + width*2;
+
+        const times = [0, width/circumference, (width + height) / circumference ,(width*2 + height)/ circumference ,1]
+
+        const options: ValueAnimationTransition = {
+            times,
+            duration: 4,
+            repeat: Infinity,
+            ease: "linear",
+            repeatType: "loop"
+        }
+
+        animate(xPercentage, [0, 100, 100, 0, 0], options);
+        animate(yPercentage, [0, 0, 100, 100, 0], options);
+    }, [props.selected]);
 
     const handleTabHover = () => {
         if(dotLottieRef.current === null) return;
@@ -46,17 +77,31 @@ const FeatureTab = (tab: typeof tabs[number]) => {
     }
 
     return (
-        <div onMouseEnter={handleTabHover} className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1">
+        <div
+            ref={tabRef} 
+            onMouseEnter={handleTabHover} 
+            className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1 relative"
+            onClick={props.onClick}
+        >
+            {props.selected && (
+                <motion.div 
+                    style={{
+                        maskImage,
+                    }}
+                    className="absolute inset-0 -m-px rounded-xl border border-[#a369ff]"
+                ></motion.div>
+            )}
+            
             <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
                 <DotLottiePlayer 
-                    src={tab.icon} 
+                    src={props.icon} 
                     className="size-5" 
-                    // autoplay
+                    autoplay
                     ref={dotLottieRef}
                 />
             </div>
-                <div className="font-medium">{tab.title}</div>
-                {tab.isNew && (<div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>)}
+                <div className="font-medium">{props.title}</div>
+                {props.isNew && (<div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>)}
             </div>
     )
 }
@@ -64,6 +109,33 @@ const FeatureTab = (tab: typeof tabs[number]) => {
 
 
 export const Features = () => {
+
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    const backgroundPositionX = useMotionValue(tabs[0].backgroundPositionX);
+    const backgroundPositionY = useMotionValue(tabs[0].backgroundPositionY);
+    const backgroundSizeX = useMotionValue(tabs[0].backgroundSizeX);
+
+    const backgroundSize = useMotionTemplate`${backgroundSizeX}% auto`;
+    const backgroundPosition = useMotionTemplate`${backgroundPositionX}% ${backgroundPositionY}%`;
+
+    const handleSelectTab = (index: number) => {
+        setSelectedTab(index);
+
+        const animateOptions: ValueAnimationTransition = {
+            duration: 2,
+            ease: "easeInOut"
+        }
+
+        animate(backgroundSizeX, [backgroundSizeX.get(), 100, tabs[index].backgroundSizeX], animateOptions);
+
+        animate(backgroundPositionX, [backgroundPositionX.get(), tabs[index].backgroundPositionX], animateOptions);
+
+        animate(backgroundPositionY, [backgroundPositionY.get(), tabs[index].backgroundPositionY], animateOptions);
+
+    }
+
+
     return (
         <section className="py-20 md:py-24">
             <div className="container">
@@ -75,19 +147,26 @@ export const Features = () => {
                 </p>
 
                 <div className="mt-10 flex flex-col gap-3 lg:flex-row">
-                    {tabs.map((tab) => (
-                        <FeatureTab {...tab} key={tab.title}/>
+                    {tabs.map((tab, tabIndex) => (
+                        <FeatureTab 
+                            {...tab} 
+                            key={tab.title} 
+                            onClick={() => handleSelectTab(tabIndex)}
+                            selected={selectedTab == tabIndex}
+                        />
                     ))}
                 </div>
 
                 <div className="border border-white/20 p-2 rounded-xl mt-3">
-                    <div 
+                    <motion.div 
                         className="aspect-video bg-cover border border-white/20 rounded-lg"
                         style={{
-                            backgroundImage: `url(${productImage.src})`
+                            backgroundImage: `url(${productImage.src})`,
+                            backgroundPosition,
+                            backgroundSize
                         }}
                     >
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
